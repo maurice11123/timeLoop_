8698cbd0e4dd1f5f5b8369fc9fbe19d2b8e13fe5
mauri <mauri@DESKTOP-GIMAHBD>
2021-09-01 14:30:22 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace timeLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+            int magicNum;
+
+            Console.WriteLine("Enter Wizard Number");
+            magicNum = int.Parse(Console.ReadLine());
+
+
+            for (i = 1; i < magicNum + 1; i++)
+            {
+                Console.WriteLine(i + " Abracadabra");
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
